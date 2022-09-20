fe665a74445f27631bfe8031c0357f432a201b5b
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-20 09:21:11 +0100

    Fix linter errors

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let booksList = [];
 
 function addToLocalStorage() {
-  let bookListSerialized = JSON.stringify(booksList);
-  localStorage.setItem("bookList", bookListSerialized);
+  const bookListSerialized = JSON.stringify(booksList);
+  localStorage.setItem('bookList', bookListSerialized);
 }
 
 function addBook(title, author) {
-  let book = {
-    title: title,
-    author: author,
+  const book = {
+    title,
+    author,
   };
   booksList.push(book);
   addToLocalStorage();
@@ -16,52 +16,50 @@ function addBook(title, author) {
 
 function removeBook(title, author) {
   booksList = booksList.filter(
-    (item) => item.title !== title && item.author !== author
+    (item) => item.title !== title && item.author !== author,
   );
   addToLocalStorage();
 }
 
 function createBookDiv(book) {
-  const div = document.createElement("div");
-  div.className = "book";
+  const div = document.createElement('div');
+  div.className = 'book';
   div.innerHTML = `<p class="author">${book.author}</p>
     <p class="title">${book.title}</p>
     <button class="remove-btn">remove</button> <hr>`;
 
-    const removeBtn = div.querySelector('.remove-btn');
-    removeBtn.addEventListener('click', () => {
-        removeBook(book.title, book.author);
-        div.remove();
-    });
+  const removeBtn = div.querySelector('.remove-btn');
+  removeBtn.addEventListener('click', () => {
+    removeBook(book.title, book.author);
+    div.remove();
+  });
   return div;
 }
 
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const form = document.getElementById("form");
-const booksSection = document.getElementById("books");
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const form = document.getElementById('form');
+const booksSection = document.getElementById('books');
 
-window.addEventListener("load", () => {
-  if (localStorage.getItem("bookList")) {
-    booksList = JSON.parse(localStorage.getItem("bookList"));
-    console.log(booksList);
-    for (let item of booksList) {
-      let newBook = createBookDiv(item);
+window.addEventListener('load', () => {
+  if (localStorage.getItem('bookList')) {
+    booksList = JSON.parse(localStorage.getItem('bookList'));
+    booksList.forEach((item) => {
+      const newBook = createBookDiv(item);
       booksSection.append(newBook);
-    }
+    });
   }
 });
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   addBook(title.value, author.value);
-  let obj = {
+  const obj = {
     title: title.value,
     author: author.value,
   };
-  title.value='';
-  author.value='';
-  let newBookDiv = createBookDiv(obj);
+  title.value = '';
+  author.value = '';
+  const newBookDiv = createBookDiv(obj);
   booksSection.append(newBookDiv);
   e.preventDefault();
 });
-
